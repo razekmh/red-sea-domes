30a9264dc053d97c8d7b21b48ef8274785cdf302
Mahmoud Abdelrazek <razekmh@gmail.com>
2023-11-11 21:51:08 +0200

    add parser for min_sigma, max_sigma, threshold, and radius_scaler with default values.

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ import numpy as np
 from skimage import feature, measure
 import matplotlib.pyplot as plt
 from pathlib import Path
+import argparse
+
+# %%
+parser = argparse.ArgumentParser(
+    prog='Red Sea Dome Detection using LoG',
+    description='This program detects the domes in the Red Sea depth map using Laplacian of Gaussian (LoG) filter.',
+    epilog='Please feel free to open an issue at the repo if you face any problem')
+
+parser.add_argument('-ns', '--min_sigma', type=int, default=0, help='Minimum standard deviation for Gaussian kernel. Default: 0')
+parser.add_argument('-xs', '--max_sigma', type=int, default=15, help='Maximum standard deviation for Gaussian kernel. Default: 15')
+parser.add_argument('-t', '--threshold', type=float, default=0.01, help='Threshold value to detect blobs. Default: 0.01')
+parser.add_argument('-rs', '--radius_scaler', type=int, default=1, help='Radius scaler to scale the sigma of the blob and use the result as a radius of the blob. Default: 1')
+
+args = parser.parse_args()
+
+min_sigma = args.min_sigma
+max_sigma = args.max_sigma
+threshold = args.threshold
+radius_scaler = args.radius_scaler
 
 # %%
 def detect_file_path():
@@ -19,20 +38,6 @@ def read_data(data_path):
 
 # %%
 def filter_islands(data):
-    # check the count of negative value vs positive values
-    # # if negative values are more then remove the positive values
-    # # if positive values are more then remove the negative values
-
-    # # count the number of negative values
-    # neg_count = np.count_nonzero(data < 0)
-    # pos_count = np.count_nonzero(data > 0)
-
-    # if neg_count > pos_count:
-        # remove positive values
-    #     data = np.where(data < 0, data, np.nan)
-    # else:
-    #     # remove negative values
-    #     data = np.where(data > 0, data, np.nan)
     data = np.where(data > 0, np.nan,  data )
     return data
 
@@ -43,83 +48,34 @@ def filter_edges(data):
 # %%
 def filter_data(data_16bit):
     print("Filtering data...")
-    # data_16bit = np.multiply(data_16bit, -1)
     data_16bit = filter_islands(data_16bit)
     data_16bit = filter_edges(data_16bit)
     return data_16bit
 # %%
 def extract_blobs(data_16bit):
     print("Extracting blobs...")
-    blobs = feature.blob_log(data_16bit, min_sigma=0, max_sigma=15, threshold=0.01)
-    #print(len(blobs))
+    blobs = feature.blob_log(data_16bit, min_sigma=min_sigma, max_sigma=max_sigma, threshold=threshold)
     return blobs
 
-# %%
-def perimeter_calc(contour):
-    assert contour.ndim == 2 and contour.shape[1] == 2, contour.shape
- 
-    shift_contour = np.roll(contour, 1, axis=0)
-    dists = np.sqrt(np.power((contour - shift_contour), 2).sum(axis=1))
-    return dists.sum()
 
-
-# %%
-def classify_blobs(blobs,data_16bit):
-    print("Classifying blobs...")
-    # Initialize lists to store circularities and non-circular blobs
-    circularities = []
-    non_circular_blobs = []
-    circular_blobs = []
-    first_countour_list = []
-    blob_counter = 0
-    for blob in blobs:
-        y, x, r = blob
-        # Find the contour of the blob
-        mask = np.zeros_like(data_16bit, dtype=np.uint8)
-        mask[int(y - r):int(y + r + 1), int(x - r):int(x + r + 1)] = 1
-        contour = measure.find_contours(mask, 0.5)[0]
-        first_countour_list.append(contour)
-
-        # Calculate circularity
-        # print(measure.moments(contour))
-        area = measure.moments(contour)[0,0]  # Corrected line
-        perimeter = perimeter_calc(contour)
-        # perimeter = measure.perimeter(contour)
-        circularity = 4 * np.pi * area / (perimeter ** 2)
-        circularities.append(circularity)
-
-        # Set a threshold for circularity to classify non-circular blobs
-        circularity_threshold = 700
-        if circularity < circularity_threshold:
-            non_circular_blobs.append(blob)
-        else:
-            circular_blobs.append(blob)
-        blob_counter += 1
-        if blob_counter % 100 == 0:
-            print("count of blobs processed: ",blob_counter)
-    return circular_blobs, non_circular_blobs
-
-def plot_blobs(data_16bit,non_circular_blobs,circular_blobs):
+def plot_blobs(data_16bit, blobs, radius_scaler):
     print("Plotting blobs...")
-    # Display the detected and non-circular blobs (for visualization)
     fig, ax = plt.subplots()
     ax.imshow(data_16bit, cmap='gray')
-
-    for blob in non_circular_blobs:
+    for blob in blobs:
         y, x, r = blob
-        c = plt.Circle((x, y), r, color='red', linewidth=2, fill=False)
+        c = plt.Circle((x, y), radius_scaler*r, color='red', linewidth=2, fill=False)
         ax.add_patch(c)
-
-    for blob in circular_blobs:
-        y, x, r = blob
-        c = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
-        ax.add_patch(c)
-
     plt.show()
 
 
 # main function
 if __name__ == "__main__":
+    print("Running dome detection with the following parameters: ")
+    print("min_sigma: ", min_sigma)
+    print("max_sigma: ", max_sigma)
+    print("threshold: ", threshold)
+    print("radius_scaler: ", radius_scaler)
     data_path = detect_file_path()
     print("Detected data path: ", data_path)
     data = read_data(data_path)
@@ -128,6 +84,4 @@ if __name__ == "__main__":
     print("Data filtered successfully")
     blobs = extract_blobs(filtered_data)
     print("Blobs extracted successfully, count of blobs: ", len(blobs))
-    circular_blobs, non_circular_blobs = classify_blobs(blobs, filtered_data)
-    print("Blobs classified successfully")
-    plot_blobs(filtered_data,non_circular_blobs,circular_blobs)
+    plot_blobs(filtered_data, blobs, radius_scaler)
